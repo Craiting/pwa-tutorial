f8e8a0214ac2ddee237f281d7f38ecca0226b67f
Craig Tingey <craig@lightningkite.com>
2017-01-23 08:55:38 -0700

    starting on service worker.

--- your-first-pwapp-master/work/scripts/app.js
+++ your-first-pwapp-master/work/scripts/app.js
@@ -344,5 +344,9 @@
     app.saveSelectedCities();
   }
 
-  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
 })();
